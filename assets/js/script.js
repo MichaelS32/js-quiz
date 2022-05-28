759418c68498ad6867c4f0c6bc1ad9adccd356fe
mschneider <michaelschneider3232@gmail.com>
2022-05-28 17:55:53 -0500

    added questions and base html

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+const start_btn = document.querySelector(".start-btn button");
+const quiz_box = document.querySelector(".quiz-box");
+const result_box = document.querySelector(".result-box");
+const options_list = document.querySelector(".que-options");
+const time_line = document.querySelector("header .time-line");
+const time_text = document.querySelector(".timer .time-remaining-text");
+const time_count = document.querySelector(".timer .timer-sec")
